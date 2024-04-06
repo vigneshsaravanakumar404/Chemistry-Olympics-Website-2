46387cbaf5be99188a23d8fef76eace2eeb76ddd
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-04-06 14:23:55 -0400

    Added Background

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,6 +4,7 @@ import './../styles/Layout.css';
 function Layout() {
     return(
         <div>
+            <div id='layout-background' />
             <p>This is the Header.</p>
             <Outlet/>
             <p>This is the Footer.</p>
