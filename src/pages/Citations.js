da14038137d6d8e2671b7266a42cdd6f74fc06d9
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-04-07 10:55:09 -0400

    Created Citations Page

---
+++ src/pages/Citations.js
@@ -0,0 +1,11 @@
+import './../styles/Citations.css';
+
+function Citations() {
+    return(
+        <div>
+            <p>This is the Citations Page.</p>
+        </div>
+    );
+}
+
+export default Citations;
