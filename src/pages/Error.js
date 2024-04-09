6a33a2e6c4313753eb246a1bf7c9aa623ff5fc70
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-04-08 21:28:03 -0400

    Created and Styled Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,7 +3,8 @@ import './../styles/Error.css';
 function Error() {
     return(
         <div>
-            <p>This is the Error Page.</p>
+            <h1 id='error-title'>Error 404</h1>
+            <p id='error-subtitle'>Page Not Found.</p>
         </div>
     );
 }
