7871f3d0970314b548ef9bb820327a94e8231d9b
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-04-17 23:49:35 -0400

    Update Image
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- src/pages/Geothermal.js
+++ src/pages/Geothermal.js
@@ -3,24 +3,31 @@ import "./../styles/Geothermal.css";
 
 // TODO: ADD ANIMATIONS AND TEXT EFFECTS
 // TODO: CHANGAE IMAGES
-const WhatIsBioEnergy = () => {
+const WhatIsgeothermalEnergy = () => {
   return (
-    <div className="what-is-bio-energy-container">
-      <div className="what-is-bio-energy-text">
-        <h2 className="what-is-bio-energy-h2">What is bio Energy?</h2>
-        <p className="what-is-bio-energy-paragraph">
-          bio energy, as the name suggests, is energy derived from the Sun’s
-          radiation. Several living organisms such as plants and algae are
-          reliant on the Sun’s radiation for their energy. bio energy for the
-          production of electricity, however, is much different from the process
-          that plants use to photosynthesize. bio energy makes up 12% of the
-          energy produced through renewable means. Even calculators make use of
-          tiny bio panels to recharge their batteries. bio energy is projected
-          to produce 45% of the total energy consumption by the year 2050.
+    <div className="what-is-geothermal-energy-container">
+      <div className="what-is-geothermal-energy-text">
+        <h2 className="what-is-geothermal-energy-h2">
+          What is geothermal Energy?
+        </h2>
+        <p className="what-is-geothermal-energy-paragraph">
+          geothermal energy, as the name suggests, is energy derived from the
+          Sun’s radiation. Several living organisms such as plants and algae are
+          reliant on the Sun’s radiation for their energy. geothermal energy for
+          the production of electricity, however, is much different from the
+          process that plants use to photosynthesize. geothermal energy makes up
+          12% of the energy produced through renewable means. Even calculators
+          make use of tiny geothermal panels to recharge their batteries.
+          geothermal energy is projected to produce 45% of the total energy
+          consumption by the year 2050.
         </p>
       </div>
-      <div className="what-is-bio-energy-image">
-        <img src="../images/Geothermal-Energy.png" alt="bio Energy" />
+      <div className="what-is-geothermal-energy-image">
+        <img
+          src="https://raw.githubusercontent.com/vigneshsaravanakumar404/Chemistry-Olympics-Website-2/3852224f15ee2e6e93542caf1d28777b7a3a27a8/src/images/Geothermal-Energy.png?token=GHSAT0AAAAAACJ6T3ZHHE2OEQMZJTZ2JX7IZRAUVWA"
+          alt="geothermal Energy"
+          style={{ marginBottom: "50px" }}
+        />
       </div>
     </div>
   );
@@ -31,37 +38,37 @@ const Benefits = () => {
     {
       title: "Renewable and Sustainable",
       description:
-        "bio energy is both renewable and sustainable in the long term as it relies on the Sun. bio energy will continue to hit the earth’s surface for several billion years, the entire lifetime of the Sun.",
+        "geothermal energy is both renewable and sustainable in the long term as it relies on the Sun. geothermal energy will continue to hit the earth’s surface for several billion years, the entire lifetime of the Sun.",
       image: "https://via.placeholder.com/500",
     },
     {
       title: "Cost Effective and Low Maintenance",
       description:
-        "Though there are initial installation costs, in the long term bio energy is both cost effective and low maintenance. With no moving or frequently worn out parts, bio energy can last, oftentimes, 30 - 35 years before having to be replaced.",
+        "Though there are initial installation costs, in the long term geothermal energy is both cost effective and low maintenance. With no moving or frequently worn out parts, geothermal energy can last, oftentimes, 30 - 35 years before having to be replaced.",
       image: "https://via.placeholder.com/500",
     },
     {
       title: "Reduces Energy Costs",
       description:
-        "Currently most houses with bio panels installed sell energy back to the grid during the day as they produce a net positive amount of energy. They can then use this money to buy power from the grid during the day. This means even during the nighttime, bio energy produced during the day will reduce energy costs.",
+        "Currently most houses with geothermal panels installed sell energy back to the grid during the day as they produce a net positive amount of energy. They can then use this money to buy power from the grid during the day. This means even during the nighttime, geothermal energy produced during the day will reduce energy costs.",
       image: "https://via.placeholder.com/500",
     },
     {
       title: "Viable Option in Remote Areas",
       description:
-        "Sometimes bio energy can be the only viable option in a certain location. In remote or rural areas, places without access to the central power grid, bio energy can be the only option.",
+        "Sometimes geothermal energy can be the only viable option in a certain location. In remote or rural areas, places without access to the central power grid, geothermal energy can be the only option.",
       image: "https://via.placeholder.com/500",
     },
     {
       title: "Clean and Environmentally Friendly",
       description:
-        "bio energy is also clean. It produces no harmful emissions of greenhouse gases.",
+        "geothermal energy is also clean. It produces no harmful emissions of greenhouse gases.",
       image: "https://via.placeholder.com/500",
     },
     {
       title: "Contributes to Renewable Energy Production",
       description:
-        "There are numerous benefits to bio energy and it is no surprise that it makes 19% of the total energy produced by renewable energy sources in the United States.",
+        "There are numerous benefits to geothermal energy and it is no surprise that it makes 19% of the total energy produced by renewable energy sources in the United States.",
       image: "https://via.placeholder.com/500",
     },
   ];
@@ -149,20 +156,21 @@ const LimitationsAndShortcomings = () => {
 };
 
 // TODO: For small devices make the images stack on top of each other, then the text
-const HowbioEnergyWorks = () => {
+const HowgeothermalEnergyWorks = () => {
   return (
     <div className="callout-box">
       <div className="callout-content">
         <div className="text-content">
-          <h2 className="callout-title">How bio Energy Works</h2>
+          <h2 className="callout-title">How geothermal Energy Works</h2>
           <p className="callout-text">
-            bio energy is produced by capturing sunlight and converting it into
-            electricity using photovoltaic cells. These cells are typically made
-            of silicon and generate direct current (DC) electricity when exposed
-            to sunlight. This electricity is then converted into alternating
-            current (AC) by inverters, making it usable for powering homes and
-            businesses. bio panels can be installed on rooftops or in open areas
-            where they can receive uninterrupted sunlight.{" "}
+            geothermal energy is produced by capturing sunlight and converting
+            it into electricity using photovoltaic cells. These cells are
+            typically made of silicon and generate direct current (DC)
+            electricity when exposed to sunlight. This electricity is then
+            converted into alternating current (AC) by inverters, making it
+            usable for powering homes and businesses. geothermal panels can be
+            installed on rooftops or in open areas where they can receive
+            uninterrupted sunlight.{" "}
             <img
               className="latex"
               src="https://math.vercel.app/?from=2H_2+%2B+O_2+%5Clongrightarrow+2H_2O"
@@ -182,16 +190,18 @@ const HowbioEnergyWorks = () => {
   );
 };
 
-function Bio() {
+function Geothermal() {
   return (
     <div>
-      <WhatIsBioEnergy />
+      <WhatIsgeothermalEnergy />
       <Benefits />
       <LimitationsAndShortcomings />
       <br />
-      <HowbioEnergyWorks />
+      <HowgeothermalEnergyWorks />
     </div>
   );
 }
 
-export default Bio;
+export default Geothermal;
+
+// https://cdn-icons-png.flaticon.com/512/3212/3212590.png
