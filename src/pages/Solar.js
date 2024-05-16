2daabb5b18ad537f09845c43c01248a0381dd6ba
Vignesh Saravanakumar <74271045+vigneshsaravanakumar404@users.noreply.github.com>
2024-05-15 23:04:31 -0400

    Update Solar.js

--- src/pages/Solar.js
+++ src/pages/Solar.js
@@ -67,9 +67,6 @@ const Benefits = () => {
     <div className="benefits-section-solar">
       <h2 className="benefits-h2-solar">Benefits</h2>
       <p className="benefits-paragraph-solar">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-        nisl ac ultrices ultricies, nunc nunc tincidunt nunc, nec tincidunt nunc
-        nunc nec.
       </p>
       <div className="benefits-cards-container-solar">
         {benefitsData.map((benefit, index) => (
